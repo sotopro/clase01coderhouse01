1a400c246d00d8f2908855dd25faaab026e3f6ae
dsoto1-rbi <dsoto1@rbi.com>
2022-05-30 20:20:20 -0500

    [feature] - add modal & functions to open modal & delete

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
-import { View, TextInput, Button, Text } from "react-native";
+import {
+  View,
+  TextInput,
+  Button,
+  Text,
+  FlatList,
+  Modal,
+  TouchableOpacity,
+} from "react-native";
 import { styles } from "./styles";
 
 export default function App() {
   const [text, setText] = useState("");
   const [list, setList] = useState([]);
+  const [itemSelected, setItemSelected] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleOnChangeText = (text) => {
     setText(text);
@@ -20,6 +30,31 @@ export default function App() {
     }
   };
 
+  const onHandlerDelete = (id) => {
+    setList((currentList) => currentList.filter((item) => item.id !== id));
+    setItemSelected({});
+    setModalVisible(!modalVisible);
+  };
+
+  const onHandlerModal = (id) => {
+    setItemSelected(list.filter((item) => item.id === id)[0]);
+    setModalVisible(!modalVisible);
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.containerItemList}>
+      <Text style={styles.itemList}>{item.value}</Text>
+      <TouchableOpacity
+        onPress={() => onHandlerModal(item.id)}
+        style={styles.deleteButton}
+      >
+        <Text style={styles.deleteButtonText}>X</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  const keyExtractor = (item) => item.id.toString();
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
@@ -32,11 +67,33 @@ export default function App() {
         />
         <Button title="ADD" onPress={() => addItem()} color="#6B4E71" />
       </View>
-      {list.map((item) => (
-        <View key={item.id}>
-          <Text style={styles.itemList}>{item.value}</Text>
-        </View>
-      ))}
+      <FlatList
+        data={list}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        style={styles.containerList}
+      />
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        onRequestClose={() => null}
+      >
+        <TouchableOpacity
+          style={styles.modalContent}
+          onPress={() => setModalVisible(!modalVisible)}
+        >
+          <View style={styles.modalTitleContainer}>
+            <Text style={styles.modalTitle}>Delete Item</Text>
+          </View>
+          <Text style={styles.modalText}>Are you sure?</Text>
+          <Text style={styles.modalMessage}>{itemSelected.value}</Text>
+          <Button
+            title="Okay"
+            onPress={() => onHandlerDelete(itemSelected.id)}
+            color="#6B4E71"
+          />
+        </TouchableOpacity>
+      </Modal>
     </View>
   );
 }
--- styles.js
+++ styles.js
@@ -16,8 +16,51 @@ export const styles = StyleSheet.create({
     borderBottomColor: "#6B4E71",
     flex: 0.85,
   },
-  itemList: {
-    marginVertical: 10,
+  containerList: {
     marginHorizontal: 20,
+  },
+  containerItemList: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  itemList: {
+    fontSize: 14,
+  },
+  deleteButton: {
+    backgroundColor: "#6B4E71",
+    paddingVertical: 10,
+    paddingHorizontal: 18,
+  },
+  deleteButtonText: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  modalContent: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+  },
+  modalTitleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 18,
+    marginVertical: 20,
+    marginHorizontal: 20,
+  },
+  modalText: {
+    fontSize: 18,
+    marginHorizontal: 20,
+  },
+  modalMessage: {
+    fontSize: 18,
+    marginHorizontal: 20,
+    marginVertical: 20,
+    fontWeight: "bold",
   },
 });
